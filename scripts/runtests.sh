33eea24def8c809511e922ce4f3a563a089b703a
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2021-03-11 15:13:39 -0600

    <Initial>[Project Setup]: Fix TravisCI

---
+++ scripts/runtests.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+function changeToProjectRoot {
+
+    export areHere=`basename ${PWD}`
+    if [[ ${areHere} = "scripts" ]]; then
+        cd ..
+    fi
+}
+
+changeToProjectRoot
+
+
+
+#python3 -m tests.TestAll $*
+#status=$?
+#
+#./scripts/cleanup.sh
+#
+#cd -  > /dev/null 2>&1
+#
+#
+#echo "Exit with status: ${status}"
+#exit ${status}
+
+echo "No tests.  Yet"
